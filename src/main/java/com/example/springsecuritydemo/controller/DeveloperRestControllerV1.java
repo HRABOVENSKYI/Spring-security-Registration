a80bb68881109b2a315e0ef6763267562df7daf1
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-08-06 18:04:49 +0300

    Added project template

---
+++ src/main/java/com/example/springsecuritydemo/controller/DeveloperRestControllerV1.java
@@ -0,0 +1,33 @@
+package com.example.springsecuritydemo.controller;
+
+import com.example.springsecuritydemo.model.Developer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+
+    private static final List<Developer> DEVELOPERS = List.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Sergey", "Sergeev"),
+            new Developer(3L, "Peter", "Petrov")
+    );
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream()
+                .filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
